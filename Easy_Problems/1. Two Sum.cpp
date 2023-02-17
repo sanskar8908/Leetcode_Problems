e4f9b60cd3190e04e25b7b8e25f0632ca149a802
Sanskar Singh <115302974+sanskar8908@users.noreply.github.com>
2023-02-17 17:36:46 +0530

    Create 1. Two Sum.cpp

---
+++ Easy_Problems/1. Two Sum.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>v=nums;
+        vector<int>ans;
+        sort(v.begin(),v.end());
+        int i=0,j=n-1;
+        int num1,num2;
+        while(i<j)
+        {
+          if(v[i]+v[j]==target)
+          {
+            num1=v[i]; 
+            num2=v[j]; 
+            break;
+          }
+          else if(v[i]+v[j]>target)
+          {
+            j--;
+          }
+          else{
+              i++;
+          }
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==num1 || nums[i]==num2 && count<2)
+            {
+               ans.push_back(i);
+               count++;
+               if(count==2)
+               {
+                 break;
+               }
+            }
+        }
+        return ans;
+    }
+};
