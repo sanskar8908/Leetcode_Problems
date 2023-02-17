59d28c7b7f63711d8cda6853616262fc7007a249
Sanskar Singh <115302974+sanskar8908@users.noreply.github.com>
2023-02-17 17:50:01 +0530

    Create 9. Palindrome Number.cpp

---
+++ Easy_Problems/9. Palindrome Number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+        if(s1==s)
+        {
+           return true;
+        }
+        return false;
+
+    }
+};
