ee7290ccf9a6c8a74d62f6ce03d3940101b5aa09
Sanskar Singh <115302974+sanskar8908@users.noreply.github.com>
2023-02-21 23:55:33 +0530

    Create 88. Merge Sorted Array

---
+++ Easy_Problems/88. Merge Sorted Array.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       int i=0,j=0;
+       vector<int>ans;
+       while(i<m && j<n)
+       {
+         if(nums1[i]<nums2[j])
+         {
+           ans.push_back(nums1[i]);
+           i++;
+         }
+         else
+         {
+              ans.push_back(nums2[j]);
+              j++;
+         }
+       } 
+       while(i<m)
+       {
+             ans.push_back(nums1[i]);
+           i++;
+       }
+       while(j<n)
+       {
+           ans.push_back(nums2[j]);
+           j++;
+       }
+       int k=0;
+       while(k<m+n)
+       {
+         nums1[k]=(ans[k]);
+         k++;
+       }
+    }
+};
