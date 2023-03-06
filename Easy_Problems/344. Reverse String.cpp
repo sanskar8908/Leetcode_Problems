7f1df24725ee2689128ada8e20d56023085fb08c
Sanskar Singh <115302974+sanskar8908@users.noreply.github.com>
2023-03-06 13:29:50 +0530

    Create 344. Reverse String.cpp

---
+++ Easy_Problems/344. Reverse String.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        for(int i=0,j=n-1;i<j;i++,j--)
+        {
+           swap(s[i],s[j]);
+        }
+    }
+};
